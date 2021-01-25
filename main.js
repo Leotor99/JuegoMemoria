ae5c4b25b559c7a9c92edc13f6b31fc8bb539d00
Leotor99 <jose251999leo@hotmail.com>
2021-01-25 17:15:15 -0500

    Adivina palabras con estilos

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const container = document.getElementById('container');
+var items = generaItems();
+tablero = cambiarPos(generaItems())
+console.log(tablero)
+var currentClick = {};
+var cant_click = 0;
+let elementos_click = []
+let styleBoton = 'bg-purple-400  h-32  sm:w-32 lg:w-32 rounded-md flex items-center justify-center text-white text-7xl font-bold hover:bg-purple-500 '
+tablero.map(symbol => {
+    const boton = document.createElement('button');
+    boton.setAttribute('id', symbol.id)
+    boton.setAttribute('symbol', symbol.symbol)
+    boton.className = styleBoton;
+    container.appendChild(boton);
+})
+
+desabilitarItems()
+mostrarOcultar()
+
+
 function generaItems() {
     let tablero = [];
     let caracter = 65;
@@ -37,7 +57,6 @@ function cambiarPos(personas) {
     return array_cambiado
 }
 
-var items = generaItems();
 function search(id, symbol) {
     let resp = {};
     items.forEach(elemento => {
@@ -48,71 +67,52 @@ function search(id, symbol) {
     return resp;
 
 }
-
-tablero = cambiarPos(generaItems())
-
 function generaNumeroAleatorio(minimo, maximo) {
 
     var num = Math.round(Math.floor(Math.random() * (minimo - (maximo + 1)) + (maximo + 1)));
     return num;
 }
 
-const container = document.getElementById('container');
-tablero.map(symbol => {
-    const boton = document.createElement('button');
-    //boton.textContent=symbol.symbol;
-    boton.setAttribute('id', symbol.id)
-    boton.setAttribute('symbol', symbol.symbol)
-    boton.style = ' font-size:100px;';
-    container.appendChild(boton);
-})
-
-var currentClick = {};
-var cant_click = 0;
-let elementos_click = []
-console.log(tablero)
-
-desabilitarItems()
-mostrarOcultar()
-
-
-
 Array.from(container.children).map(child => {
-    child.addEventListener('click', function (e) {
+    child.addEventListener('click',  function (e) {
+        
         cant_click++;
         let elemento = search(e.target.getAttribute('id'), e.target.getAttribute('symbol'));
         e.target.textContent = elemento.symbol;
-         e.target.setAttribute('disabled',true);
-         let item1 = e.target;
-         let item2 = currentClick.target;
+        e.target.setAttribute('disabled',true);
+        let item1 = e.target;
+        // e.target.classList.add('animate__animated','animate__flash')
+
+        
+        
+        let item2 = currentClick.target;        
         if (cant_click == 2) {
+            
             desabilitarItems()
-            // console.log(item1.getAttribute('id'),item2.getAttribute('id'))
-            // console.log(item1.getAttribute('symbol'),item2.getAttribute('symbol'))
+            //Si coinciden los simbolos
             if (item1.getAttribute('symbol') === item2.getAttribute('symbol')) {
-                item1.style = 'border:2px solid green; font-size:100px;';
-                item2.style = 'border:2px solid green; font-size:100px;';
+                
+                item1.className = styleBoton+'border-4 border-green-400' ;
+                item2.className = styleBoton+'border-4 border-green-400' ;
                 elementos_click.push(item1)
                 elementos_click.push(item2)
                 habilitarItems(elementos_click)
             } else {
-                item1.style = 'border:2px solid red; font-size:100px;';
-                item2.style = 'border:2px solid red; font-size:100px;';
+            //Si no coinciden los simbolos
+            
+                item1.className = styleBoton+'border-4 border-red-400' ;
+                item2.className = styleBoton+'border-4 border-red-400' ;
                 setTimeout(()=>{
-                    item1.style = 'font-size:100px;';
-                    item2.style = 'font-size:100px;';
+                    item1.className = styleBoton;
+                    item2.className = styleBoton;
                     item1.textContent='';
                     item2.textContent='';
                     habilitarItems(elementos_click)
                 },3000)
-                
             }
-
-            console.log('ya hay dos click', item1, item2);
-
             cant_click = 0;
         }
-
+        
         currentClick = e;
     })
 })
@@ -133,9 +133,7 @@ async function habilitarItems(array) {
 
 async function desabilitarItems() {
     Array.from(container.children).map(child => {
-
         child.disabled = true
-
     })
 }
 
@@ -145,23 +143,19 @@ function mostrarOcultar(){
     var miliseconstotal = 0;
     Array.from(container.children).map(child => {
         setTimeout(()=>{
+            
             child.textContent=child.getAttribute('symbol')
         },milisecons)
         milisecons=milisecons+1000;
-        
         miliseconstotal=miliseconstotal+milisecons
     })
-    
-
     Array.from(container.children).reverse().map(child => {
         setTimeout(()=>{
+           
             child.textContent=''
         },milisecons)
         milisecons=milisecons+1000;
         miliseconstotal=miliseconstotal+milisecons
-
-
-
     })
     console.log(miliseconstotal)
     Array.from(container.children).map(child => {
@@ -170,10 +164,6 @@ function mostrarOcultar(){
         },26000)
 
     })
-
-
-
-
 }
 
 
